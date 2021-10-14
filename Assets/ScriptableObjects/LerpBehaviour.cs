71abe76b0d9855ce0b9d144cd60ec8ff8d4c4410
Sai <thegeekskm101@gmail.com>
2021-10-14 10:35:15 -0500

    Finished Lerp Maker
    As I made this thing, I realized how useless it was...I could've just made a component.

---
+++ Assets/ScriptableObjects/LerpBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LerpBehaviour : MonoBehaviour
+{
+    //variables
+    [SerializeField] public Transform firstTransform;
+    [SerializeField] public Transform secondTransform;
+    [SerializeField] public float movementDuration = 3f;
+    
+
+    private float elapsedTime;
+
+    void Start()
+    {
+        transform.position = firstTransform.position;
+    }
+
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        float movementPercentage = elapsedTime / movementDuration;
+
+        transform.position = Vector3.Lerp(firstTransform.position, secondTransform.position, movementPercentage);
+    }
+
+    public void Remove()
+    {
+        Destroy(this);
+
+    }
+}
--- Assets/Scripts/LerpEditorScript.cs
+++ Assets/Scripts/LerpEditorScript.cs
@@ -7,136 +7,287 @@ using UnityEditor;
 
 public class LerpEditorScript : EditorWindow
 {
+
+     #region Window Variables
     Texture2D headerSectionTexture;
-    Texture2D leftTransformTexture;
-    Texture2D rightTransformTexture;
+    Texture2D firstTexture;
+    Texture2D secondTexture;
     Texture2D calculateAreaTexture;
 
     Color headerSectionColor = new Color(0, 0.5f, 1, 0.2f);
-    Color leftTransformColor = new Color(0.5f, 0, 1, 0.5f);
-    Color rightTransformColor = new Color(1, 0.2f, 0, 0.5f);
+    Color firstColor = new Color(0.5f, 0, 1, 0.5f);
+    Color secondColor = new Color(1, 0.2f, 0, 0.5f);
     Color calculateAreaColor = new Color(1, 0.92f, 0.016f, 1);
 
     Rect headerSectionRect;
-    Rect leftTransformRect;
-    Rect rightTransformRect;
+    Rect firstRect;
+    Rect secondRect;
     Rect calculateAreaRect;
+     #endregion
+
+     #region Actual Variables
+     
+     Transform firstTransform;
+     Transform secondTransform;
+
+     GameObject affectedObject;
+     float desiredDuration;
+     
 
 
+     public Transform FirstTransform
+     {
+          get
+          {
+               return firstTransform;
+          }
+          set
+          {
+               firstTransform = value;
+          }
+     }
+     public Transform SecondTransform
+     {
+          get
+          {
+               return secondTransform;
+          }
+          set
+          {
+               secondTransform = value;
+          }
+     }
 
+    
+
+     public float DesiredDuration
+     {
+          get
+          {
+               return desiredDuration;
+          }
+          set
+          {
+               desiredDuration = value;
+          }
+     }
+
+     #endregion
+
+    
+
+    
+
+     #region WindowCommands
     [MenuItem("Window/Lerp")]
-       static void OpenWindow()
-       {
-           LerpEditorScript window = (LerpEditorScript)GetWindow(typeof(LerpEditorScript));
-           window.minSize = new Vector2(300, 500);
-           window.maxSize = new Vector2(600, 1000);
-           window.Show();
-       }
+     static void OpenWindow()
+      {
+          LerpEditorScript window = (LerpEditorScript)GetWindow(typeof(LerpEditorScript));
+          window.minSize = new Vector2(300, 500);
+          window.maxSize = new Vector2(600, 1000);
+          window.Show();
+      }
+     void OnEnable()
+     {
+          InitTextures();    
+          InitData();
+     }
+     
+     public static void InitData()
+     {
+          //lerpBehaviour = (LerpBehaviour)ScriptableObject.CreateInstance(typeof(LerpBehaviour));
+     }
 
-       void OnEnable()
-       {
-            InitTextures();    
-       }
+     void InitTextures()
+     {
+          headerSectionTexture = new Texture2D(1, 1);
+          headerSectionTexture.SetPixel(0, 0, headerSectionColor);
+          headerSectionTexture.Apply();
+          
+          firstTexture = new Texture2D(1, 1);
+          firstTexture.SetPixel(0, 0, firstColor);
+          firstTexture.Apply();
+          
+          secondTexture = new Texture2D(1, 1);
+          secondTexture.SetPixel(0, 0, secondColor);
+          secondTexture.Apply();
+          
+          calculateAreaTexture = new Texture2D(1, 1);
+          calculateAreaTexture.SetPixel(0, 0, calculateAreaColor);
+          calculateAreaTexture.Apply();
+     }
+     void OnGUI()
+     {
+          DrawLayouts();
+          DrawHeader();
+          DrawfirstSettings();
+          DrawsecondSettings();
+          DrawCalculateAreaSettings();
+     }
 
-       void InitTextures()
-       {
-            headerSectionTexture = new Texture2D(1, 1);
-            headerSectionTexture.SetPixel(0, 0, headerSectionColor);
-            headerSectionTexture.Apply();
+     void DrawLayouts()
+     {
+          headerSectionRect.x = 0;
+          headerSectionRect.y = 0;
+          headerSectionRect.width = Screen.width;
+          headerSectionRect.height = 50;
 
-            leftTransformTexture = new Texture2D(1, 1);
-            leftTransformTexture.SetPixel(0, 0, leftTransformColor);
-            leftTransformTexture.Apply();
+          GUI.DrawTexture(headerSectionRect, headerSectionTexture);
 
-            rightTransformTexture = new Texture2D(1, 1);
-            rightTransformTexture.SetPixel(0, 0, rightTransformColor);
-            rightTransformTexture.Apply();
+          firstRect.x = 0;
+          firstRect.y = 50;
+          firstRect.width = Screen.width / 2f;
+          firstRect.height = Screen.height - 150;
 
-            calculateAreaTexture = new Texture2D(1, 1);
-            calculateAreaTexture.SetPixel(0, 0, calculateAreaColor);
-            calculateAreaTexture.Apply();
-       }
+          GUI.DrawTexture(firstRect, firstTexture);
 
-       void OnGUI()
-       {
-            DrawLayouts();
-            DrawHeader();
-            DrawLeftTransformSettings();
-            DrawRightTransformSettings();
-            DrawCalculateAreaSettings();
-       }
+          secondRect.x = Screen.width / 2f;
+          secondRect.y = 50;
+          secondRect.width = Screen.width / 2f;
+          secondRect.height = Screen.height - 150;
+       
 
-       void DrawLayouts()
-       {
-            headerSectionRect.x = 0;
-            headerSectionRect.y = 0;
-            headerSectionRect.width = Screen.width;
-            headerSectionRect.height = 50;
+          GUI.DrawTexture(secondRect, secondTexture);
 
-            GUI.DrawTexture(headerSectionRect, headerSectionTexture);
-
-            leftTransformRect.x = 0;
-            leftTransformRect.y = 50;
-            leftTransformRect.width = Screen.width / 2f;
-            leftTransformRect.height = Screen.height - 150;
-
-            GUI.DrawTexture(leftTransformRect, leftTransformTexture);
-
-            rightTransformRect.x = Screen.width / 2f;
-            rightTransformRect.y = 50;
-            rightTransformRect.width = Screen.width / 2f;
-            rightTransformRect.height = Screen.height - 150;
+          calculateAreaRect.x = 0;
+          calculateAreaRect.y = Screen.height - 150;
+          calculateAreaRect.width = Screen.width;
+          calculateAreaRect.height = Screen.height;
             
 
-            GUI.DrawTexture(rightTransformRect, rightTransformTexture);
-
-            calculateAreaRect.x = 0;
-            calculateAreaRect.y = Screen.height - 150;
-            calculateAreaRect.width = Screen.width;
-            calculateAreaRect.height = Screen.height;
-            
-
-            GUI.DrawTexture(calculateAreaRect, calculateAreaTexture);
+          GUI.DrawTexture(calculateAreaRect, calculateAreaTexture);
 
 
 
 
-       }
-
-       void DrawHeader()
-       {
+     }
+     void DrawHeader()
+     {
           GUILayout.BeginArea(headerSectionRect);
-          
+
           GUILayout.Label("Lerp Menu");
+         
+          GUILayout.EndArea();
+     }
+
+     void DrawfirstSettings()
+     {
+     GUILayout.BeginArea(firstRect);
+               
+          GUILayout.Label("First Transform Menu");
+
+          EditorGUILayout.BeginHorizontal();
+               
+               GUILayout.Label("First Transform");
+               firstTransform = (Transform)EditorGUILayout.ObjectField(firstTransform, typeof(Transform), true);
           
-          GUILayout.EndArea();
-       }
+          EditorGUILayout.EndHorizontal();
 
-        void DrawLeftTransformSettings()
-        {
-          GUILayout.BeginArea(leftTransformRect);
+          GUILayout.EndArea();
+     }
+
+     void DrawsecondSettings()
+     {
+          GUILayout.BeginArea(secondRect);
+          
+          GUILayout.Label("Second Transform Menu");
+
+
+          EditorGUILayout.BeginHorizontal();
                
-               GUILayout.Label("Left Transform Menu");
+               GUILayout.Label("Second Transform");
+               secondTransform = (Transform)EditorGUILayout.ObjectField(secondTransform, typeof(Transform), true);
+          
+          EditorGUILayout.EndHorizontal();
 
           GUILayout.EndArea();
-        }
+     }
 
-        void DrawRightTransformSettings()
-        {
-          GUILayout.BeginArea(rightTransformRect);
-               
-               GUILayout.Label("Right Transform Menu");
-
-          GUILayout.EndArea();
-        }
-
-        void DrawCalculateAreaSettings()
+     void DrawCalculateAreaSettings()
         {
           GUILayout.BeginArea(calculateAreaRect);
 
-              GUILayout.Label("Calculate Area Menu");
+          GUILayout.Label("Affected Object Transform Menu");
+
+          EditorGUILayout.BeginHorizontal();
+               
+               GUILayout.Label("Affected Object");
+               affectedObject = (GameObject)EditorGUILayout.ObjectField(affectedObject, typeof(GameObject), true);
+               
+          
+          EditorGUILayout.EndHorizontal();
+
+           EditorGUILayout.BeginHorizontal();
+
+               GUILayout.Label("Desired Duration (seconds)");
+               desiredDuration = (float)EditorGUILayout.FloatField(desiredDuration);
+
+          EditorGUILayout.EndHorizontal();
+
+
+          //button
+          if (GUILayout.Button("Set Up"))
+          {
+               Calculate();
+          }
+
+          if (GUILayout.Button("Remove Lerp"))
+          {
+               
+          }
 
           GUILayout.EndArea();
         }
+     
+     #endregion
    
+     #region LerpCalculation
+
+     void Calculate()
+     {
+          affectedObject.transform.position = firstTransform.position;
+          if (!affectedObject.GetComponent<LerpBehaviour>())
+          {
+               affectedObject.AddComponent<LerpBehaviour>();
+          }
+          affectedObject.GetComponent<LerpBehaviour>().firstTransform = this.firstTransform;
+          affectedObject.GetComponent<LerpBehaviour>().secondTransform = this.secondTransform;
+          affectedObject.GetComponent<LerpBehaviour>().movementDuration = this.desiredDuration;
+                   
+     }
+
+     void RemoveLerp()
+     {
+          if (affectedObject.GetComponent<LerpBehaviour>())
+          {
+               LerpBehaviour toBeGone = affectedObject.GetComponent<LerpBehaviour>();
+               Destroy(toBeGone);
+          }
+     }
+
+     #endregion
 }
+
+#region Igonore
+// public class TimeSettings : EditorWindow
+// {
+     
+
+//      public static void OpenWindow()
+//      {
+//           TimeSettings window = (TimeSettings)GetWindow(typeof(TimeSettings));
+//           window.minSize = new Vector2(250, 200);
+//           window.Show();
+//      }
+
+//      void OnGUI()
+//      {
+//           EditorGUILayout.BeginHorizontal();
+
+//                GUILayout.Label("Desired Duration (seconds)");
+//                desiredDuration = (float)EditorGUILayout.FloatField(desiredDuration);
+
+//           EditorGUILayout.EndHorizontal();
+//      }
+// }
+#endregion
