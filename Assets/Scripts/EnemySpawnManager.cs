95ede7e93b010e8267fd64fa4af5c7dc2f7d78c0
Sai <thegeekskm101@gmail.com>
2021-10-18 15:50:53 -0500

    Casual Changes

---
+++ Assets/Scripts/EnemySpawnManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnManager : MonoBehaviour
+{
+    //This is the list of all the enemies that can spawn:
+    [SerializeField] List<GameObject> _enemies = new List<GameObject>();
+
+    public List<GameObject> Enemies
+    {
+        get
+        {
+            return _enemies;
+        }
+        set
+        {
+            _enemies = value;
+        }
+    }
+    
+
+}
